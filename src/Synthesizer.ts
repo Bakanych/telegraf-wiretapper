37377b013744cf95e93761d18b004b86bc850a3a
Bakanych <manych@gmail.com>
2019-04-12 19:42:36 +0800

    removed empty axios catch

--- src/Synthesizer.ts
+++ src/Synthesizer.ts
@@ -1,37 +1,41 @@
-import axios from 'axios';
-import formurlencoded from 'form-urlencoded';
+import axios from "axios";
+import formurlencoded from "form-urlencoded";
 
 export enum Voice {
-  Oksana = 'oksana',
-  Alyss = 'alyss',
-  Jane = 'jane',
-  Omazh = 'omazh',
-  Zahar = 'zahar',
-  Ermil = 'ermil'
+  Oksana = "oksana",
+  Alyss = "alyss",
+  Jane = "jane",
+  Omazh = "omazh",
+  Zahar = "zahar",
+  Ermil = "ermil"
 }
 
-export const emotions = ['neutral'];
+export const emotions = ["neutral"];
 
 export interface Synthesizer {
-  getPause(n?: number): string,
-  synthesize(text: string, language: 'ru-RU' | 'en-US', voice?: Voice, emotion?: string): Promise<Buffer | undefined>;
+  getPause(n?: number): string;
+  synthesize(
+    text: string,
+    language: "ru-RU" | "en-US",
+    voice?: Voice,
+    emotion?: string
+  ): Promise<Buffer | undefined>;
 }
 
 export class YandexTextToSpeech implements Synthesizer {
-
-  constructor(
-    private accessKey: string,
-    private folderId: string) {
+  constructor(private accessKey: string, private folderId: string) {
     this.setToken();
   }
 
-  private token = { expiresAt: 0, iamToken: '' };
+  private token = { expiresAt: 0, iamToken: "" };
 
-  getPause = (n: number = 2) => [...Array(n).keys()].map(x => '-').join(',') + ' ';
+  getPause = (n: number = 2) =>
+    [...Array(n).keys()].map(x => "-").join(",") + " ";
 
   private async setToken(): Promise<void> {
-    const url = 'https://iam.api.cloud.yandex.net/iam/v1/tokens';
-    return axios.post(url, { "yandexPassportOauthToken": this.accessKey })
+    const url = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
+    return axios
+      .post(url, { yandexPassportOauthToken: this.accessKey })
       .then(r => {
         this.token = {
           expiresAt: new Date(r.data.expiresAt).getTime(),
@@ -41,33 +45,35 @@ export class YandexTextToSpeech implements Synthesizer {
     //.catch(e => { console.log(e.response.data); return null; });
   }
 
-
-  async synthesize(text: string, language: 'ru-RU' | 'en-US', voice: Voice = Voice.Oksana, emotion = 'neutral', format = 'oggopus'): Promise<Buffer | undefined> {
-
+  async synthesize(
+    text: string,
+    language: "ru-RU" | "en-US",
+    voice: Voice = Voice.Oksana,
+    emotion = "neutral",
+    format = "oggopus"
+  ): Promise<Buffer | undefined> {
     if (!this.token.iamToken || this.token.expiresAt < Date.now()) {
       await this.setToken();
     }
 
-    const url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize';
+    const url = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize";
     const headers = {
-      "Authorization": `Bearer ${this.token.iamToken}`
+      Authorization: `Bearer ${this.token.iamToken}`
     };
     const data = formurlencoded({
-      "format": format,
-      "voice": voice,
-      "emotion": emotion,
-      "sampleRateHertz": "48000",
-      "folderId": this.folderId,
-      "text": text,
-      "lang": language
+      format: format,
+      voice: voice,
+      emotion: emotion,
+      sampleRateHertz: "48000",
+      folderId: this.folderId,
+      text: text,
+      lang: language
     });
 
-    return axios.post(url, data, { headers: headers, responseType: 'arraybuffer' })
+    return axios
+      .post(url, data, { headers: headers, responseType: "arraybuffer" })
       .then(response => {
         return response.data;
-      })
-      .catch(async err => {
-        console.log(`${err.response.status}: ${err.response.statusText}`);
       });
   }
 }
